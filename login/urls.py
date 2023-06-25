c456f4698f96c82e1ba13578f490dd117fc696c8
Marat <maratsafin.let@gmail.com>
2023-06-25 20:11:26 +0200

    init

--- login/urls.py
+++ login/urls.py
@@ -1,11 +1,12 @@
 
 from django.urls import path
 
-from login.views import LoginView, LogoutView
+from login.views import LoginView, LogoutView, CreateAccountView
 
 app_name = 'login'
 
 urlpatterns = [
     path('', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view, name='logout'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('create/', CreateAccountView.as_view(), name='create'),
 ]
--- login/views.py
+++ login/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
-
+from .forms import CustomUserCreationForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -29,3 +30,21 @@ class LogoutView(View):
         if request.user.is_authenticated:
             logout(request)
         return redirect('store:shop')
+
+
+class CreateAccountView(View):
+    def get(self, request):
+        return render(request, "login/create_account.html")
+
+    def post(self, request):
+        form = CustomUserCreationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password1')
+            user = User.objects.create_user(username=username, email=email, password=password)
+            user.save()
+            login(request, user)
+            return redirect('store:shop')
+        return render(request, 'login/create_account.html',
+                      context={'errors': form.errors})
